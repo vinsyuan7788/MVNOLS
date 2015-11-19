ed2932ee5f2b0577bdeee2800eb2a0cc0e2ca6c4
vinsyuan7788 <vinsyuan7788@gmail.com>
2015-11-19 02:12:51 -0500

    This is version 1.0.1

--- MVNOLS/src/main/java/cn/itcast/global/interceptor/LoginValidator.java
+++ MVNOLS/src/main/java/cn/itcast/global/interceptor/LoginValidator.java
@@ -21,7 +21,7 @@ public class LoginValidator implements HandlerInterceptor {
 	/**
 	 * 	Object handler: This is the H(Handler) found by HM, which only contains 1 target method mapped by request URL
 	 *  -- This target method is namely the handler|action method that will process the request (according to the SpringMVC request processing workflow)
-	 *  -- To get this method: use ((HandlerMethod) handler).getMethod()
+	 *  -- To get this method: use "((HandlerMethod) handler).getMethod()"
 	 * 	When to execute this method:
 	 *  -- Before executing the method of handler
 	 *     -- False: means intercept
@@ -55,7 +55,7 @@ public class LoginValidator implements HandlerInterceptor {
 	/**
 	 * 	Object handler: This is the H found by HM, which only contains 1 target method mapped by request URL
 	 *  -- This target method is namely the handler|action method that will process the request (according to the SpringMVC request processing workflow)
-	 *  -- To get this method: use ((HandlerMethod) handler).getMethod()
+	 *  -- To get this method: use "((HandlerMethod) handler).getMethod()"
 	 * 	When to execute this method:
 	 *  -- After executing the method of H|AC, before returning ModelAndView
 	 * 	In what situation to use:
@@ -73,7 +73,7 @@ public class LoginValidator implements HandlerInterceptor {
 	/**
 	 * 	Object handler: This is the H found by HM, which only contains 1 target method mapped by request URL
 	 *  -- This target method is namely the handler|action method that will process the request (according to the SpringMVC request processing workflow)
-	 *  -- To get this method: use ((HandlerMethod) handler).getMethod()
+	 *  -- To get this method: use "((HandlerMethod) handler).getMethod()"
 	 * 	When to execute this method:
 	 *  -- After completing execution of method in H (already returning ModelAndView)
 	 * 	In what situation to use:
--- MVNOLS/src/main/java/cn/itcast/global/interceptor/StatisticsCollector.java
+++ MVNOLS/src/main/java/cn/itcast/global/interceptor/StatisticsCollector.java
@@ -14,7 +14,7 @@ public class StatisticsCollector implements HandlerInterceptor {
 	/**
 	 * 	Object handler: This is the H(Handler) found by HM, which only contains 1 target method mapped by request URL
 	 *  -- This target method is namely the handler|action method that will process the request (according to the SpringMVC request processing workflow)
-	 *  -- To get this method: use ((HandlerMethod) handler).getMethod()
+	 *  -- To get this method: use "((HandlerMethod) handler).getMethod()"
 	 * 	When to execute this method:
 	 *  -- Before executing the method of handler
 	 *     -- False: means intercept
@@ -37,7 +37,7 @@ public class StatisticsCollector implements HandlerInterceptor {
 	/**
 	 * 	Object handler: This is the H found by HM, which only contains 1 target method mapped by request URL
 	 *  -- This target method is namely the handler|action method that will process the request (according to the SpringMVC request processing workflow)
-	 *  -- To get this method: use ((HandlerMethod) handler).getMethod()
+	 *  -- To get this method: use "((HandlerMethod) handler).getMethod()"
 	 * 	When to execute this method:
 	 *  -- After executing the method of H|AC, before returning ModelAndView
 	 * 	In what situation to use:
@@ -55,7 +55,7 @@ public class StatisticsCollector implements HandlerInterceptor {
 	/**
 	 * 	Object handler: This is the H found by HM, which only contains 1 target method mapped by request URL
 	 *  -- This target method is namely the handler|action method that will process the request (according to the SpringMVC request processing workflow)
-	 *  -- To get this method: use ((HandlerMethod) handler).getMethod()
+	 *  -- To get this method: use "((HandlerMethod) handler).getMethod()"
 	 * 	When to execute this method:
 	 *  -- After completing execution of method in H (already returning ModelAndView)
 	 * 	In what situation to use:
