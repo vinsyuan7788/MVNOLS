aa5fed62545f985830b0b0f0ae622e46b2be18f7
vinsyuan7788 <vinsyuan7788@gmail.com>
2015-11-21 12:53:17 -0500

    This is version 1.0.4

---
+++ MVNOLS/src/main/java/cn/itcast/test/dao/mapper/OptimizationMapper.java
@@ -0,0 +1,40 @@
+package cn.itcast.test.dao.mapper;
+
+import cn.itcast.item.bean.Item;
+import cn.itcast.item.bean.ItemExample;
+import cn.itcast.item.bean.wrapper.PageItemBean;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface OptimizationMapper {
+    int countByExample(ItemExample example);
+
+    int deleteByExample(ItemExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Item record);
+
+    int insertSelective(Item record);
+
+    List<Item> selectByExampleWithBLOBs(ItemExample example);
+
+    List<Item> selectByExample(ItemExample example);
+
+    Item selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Item record, @Param("example") ItemExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Item record, @Param("example") ItemExample example);
+
+    int updateByExample(@Param("record") Item record, @Param("example") ItemExample example);
+
+    int updateByPrimaryKeySelective(Item record);
+
+    int updateByPrimaryKeyWithBLOBs(Item record);
+
+    int updateByPrimaryKey(Item record);
+	
+	List<Item> queryItemListByCriteria(PageItemBean pageItemBean);
+}
--- MVNOLS/src/test/java/cn/itcast/test/others/TestQueryBean.java
+++ MVNOLS/src/test/java/cn/itcast/test/others/TestQueryBean.java
@@ -11,11 +11,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.itcast.item.bean.Item;
 import cn.itcast.item.bean.wrapper.PageItemBean;
-import cn.itcast.item.dao.mapper.ItemMapper;
 import cn.itcast.temporary.bean.query.bean.QueryBean;
 import cn.itcast.temporary.bean.query.utils.ConditionField;
 import cn.itcast.temporary.bean.query.utils.OrderField;
 import cn.itcast.temporary.bean.query.utils.QueryField;
+import cn.itcast.test.dao.mapper.OptimizationMapper;
 
 /**
  * 	This class is to test the general optimization for MyBatis DAO (i.e the optimal query bean can be applied on all JavaBeans instead of for a specific module)
@@ -26,8 +26,13 @@ import cn.itcast.temporary.bean.query.utils.QueryField;
  *  2. In order by part:
  *     -- Multiple fields order in ascending & descending should be: order by xxx, zzz, aaa ASC, ddd, yyy, bbb, ccc desc
  *     -- Hence the property in OrderField should be a single string that can accept & concatenate the input according to ascending or descending requirement
- *  3. This optimization is only workable for single-table query, not workable for multiple-table query
- * @author Administrator
+ *  3. This optimization should be done in the late-stage of the development
+ *  4. This optimization is only workable for single-table query, not workable for multiple-table query
+ *  5. This optimization requires the column name in database must be identical to the field name in JavaBean
+ *     -- See "testQueryBeanDao()" method
+ *     -- Otherwise ${fieldName} cannot be used & all the necessary fields must be manually assigned
+ *     
+ *  Hence the idea is adoptable, but not implementable during the development due to the high requirement & low cross-module generality
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = "classpath:spring/applicationContext.xml")
@@ -91,16 +96,16 @@ public class TestQueryBean {
 	 * 	Test the interaction with database: fail
 	 */
 	@Resource
-	private ItemMapper itemMapper;
+	private OptimizationMapper optimizationMapper;
 	@Test
 	public void testQueryBeanDao () throws Exception {
 		
 		PageItemBean pageItemBean = new PageItemBean();
-		pageItemBean.setQueryFieldName(null);
+		pageItemBean.setQueryFieldName("itemName");
 		pageItemBean.setConditionField("itemState", "active", false);
 		pageItemBean.setOrderField("itemPrice", true);
 		
-		List<Item> items = itemMapper.queryItemListByCriteria_Test(pageItemBean);
+		List<Item> items = optimizationMapper.queryItemListByCriteria(pageItemBean);
 		System.out.println(items.size());
 	}
 }
